4002d50b9bdce2fee74813517f74642a424252c6
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-20 20:04:10 -0300

    [CREATED] - Fastify Routes and Plugins

---
+++ src/routes/transactions.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+
+
+export async function transactionsRoutes(app: FastifyInstance){ 
+
+  app.get('/hello', async () => {
+    const transactions = await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+  
+    }).returning('*');
+  
+    return transactions
+  })
+}
