1273fdeb8ed4619efd06be75d091f6dd6809ea2c
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-20 20:45:57 -0300

    [CREATING] - New routes in transactions Listing and listing the ID of a transaction.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,31 @@ import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance){ 
 
+  // List Transactions
+  app.get('/', async () => {
+    const transactions = await knex('transactions'). select('*')
+
+    return {
+      transactions
+    }
+  })
+
+  //Busca detalhes de uma transação única.
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { 
+      transaction
+    }
+  })
+
+  //Create Transaction
   app.post('/', async (request, reply) => {
     try {
       //{Title, amount, type: credit ou debit}
