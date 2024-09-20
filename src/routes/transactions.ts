8cef64cfc71c728aaa4527391552948057ff13cb
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-20 20:22:39 -0300

    [CREATED] - First transaction made and analyzed on Insomnia

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,17 +1,35 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import crypto from "node:crypto";
 import { knex } from "../database";
 
 
 export async function transactionsRoutes(app: FastifyInstance){ 
 
-  app.get('/hello', async () => {
-    const transactions = await knex('transactions').insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-  
-    }).returning('*');
-  
-    return transactions
+  app.post('/', async (request, reply) => {
+    try {
+      //{Title, amount, type: credit ou debit}
+      
+      const createTranjsactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit'])
+      })
+
+      const { title, amount, type } = createTranjsactionBodySchema.parse(request.body);
+      
+      await knex('transactions')
+       .insert({
+         id: crypto.randomUUID(),
+         title,
+         amount: type === 'credit' ? amount : amount * -1,
+       })
+
+      // HTTP Codes
+      return reply.status(201).send()
+
+    } catch (error) {
+      console.log(" deu ruim rapaziada")
+    }
   })
 }
