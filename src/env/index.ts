d02f939f53ee27639c9c23fab11233e2e81ac769
joao.malfatti <joao.cursos1210@gmail.com>
2024-09-21 11:03:10 -0400

    [CREATING] - The test packages were downloaded and we tested the transaction creation

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import "dotenv/config"
 import { z } from "zod"
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'teste']).default('production'),
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,7 @@
-import fastify from "fastify";
-import cookie from "@fastify/cookie";
-
-import { env } from "./env";
-import { transactionsRoutes } from './routes/transactions';
+import { app } from './app';
+import { env } from './env';
 
 
-const app = fastify();
-
-app.register(cookie)
-app.register(transactionsRoutes, {
-  prefix: '/transactions',
-});
 
 app.listen({
   port: env.PORT,
