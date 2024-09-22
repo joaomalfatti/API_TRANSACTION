0ed9ea09b462197a94832e29e50acf9ddf3fec5a
joaomalfatti <joao.cursos1210@gmail.com>
2024-09-22 01:26:30 -0300

    [CREATION] - Of a new test to list all transaction with cookies.

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, test, describe } from "vitest";
+import { afterAll, beforeAll, test, describe, expect } from "vitest";
 import request from "supertest";
 import { app } from "../src/app";
 
@@ -18,12 +18,44 @@ describe('Transactions Routes', () => {
   test("User can create a new transaction", async () => {
     
     await request(app.server)
-    .post("/transactions")
-    .send({
+      .post("/transactions")
+      .send({
+          title: 'New transaction',
+          amount: 1000,
+          type: 'credit',
+      })
+      .expect(200)
+  })
+
+  test("User can list all transactions", async () => {
+
+    try {
+      const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
         title: 'New transaction',
         amount: 1000,
         type: 'credit',
-    })
-    .expect(200)
+      })
+      
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+    
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+      
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 1000,
+      })
+    ])
+
+    } 
+    catch (error) {
+      console.error(error)
+    }
   })
+  
 })
